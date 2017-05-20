aab9a5ade5651288a5c35cf0295aa3f794936707
ekesas <ekesas@gmail.com>
2017-05-20 17:04:49 -0500

    First Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  def index
+
+    @posts=Post.all
+    
+  end
+
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+      @post=Post.new(post_params)
+      if @post.save 
+        redirect_to posts_path, :notice => "Post saved!"
+      else
+          render "new" 
+      end     
+  end
+
+  def show
+    @post=Post.find(params[:id])
+  end
+
+  def update
+    @post=Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to post_path, :notice => "Your post was updated"
+
+    else
+      render "edit"
+    end 
+  end
+
+  def destroy
+  end
+
+  def edit
+
+    @post=Post.find(params[:id])
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
